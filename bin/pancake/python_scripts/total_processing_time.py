207d8430d8e1ffaa72270c9f518df8e9aeb5891f
d-maltos <dmmaltos@alaska.edu>
2023-09-05 19:56:30 +0000

    Updating total_processing_time.py script

--- bin/pancake/python_scripts/total_processing_time.py
+++ bin/pancake/python_scripts/total_processing_time.py
@@ -8,18 +8,22 @@ directory = '/mnt/raid/stockpile/distro/stats'
 proccesing_time_files = [file for file in os.listdir(directory) if file.endswith('total_processing_time_by_day.csv')]
 
 combined_dfs = []
-skip_first = True 
+skip_first = True
+count = 0
 
 for proccesing_time_file in proccesing_time_files:
-    if skip_first:
+    count += 1
+    if skip_first and count == 1:
         skip_first = False
         continue
+    if count == len(proccesing_time_files):
+        break
     file_path = os.path.join(directory, proccesing_time_file)
     df = pd.read_csv(file_path)
     df['source'] = os.path.splitext(proccesing_time_file)[0]
     combined_dfs.append(df)
 
 combined_df = pd.concat(combined_dfs, ignore_index=True)
-
+ 
 output_csv_file = 'total_processing_time_by_day_combined.csv'
 combined_df.to_csv(output_csv_file, index=False)
